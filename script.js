7f91105ce683e3e0678521914d0bae40fa99ca1a
Relax-29 <relax9690@gmail.com>
2025-07-30 12:45:52 +0530

    Animations add

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
-var tl = gsap.timeline();
-    gsap.set(".animation", {
+function page1animation() {
+    const tl2 = gsap.timeline();
+  
+    tl2.from('#nav', {
+      y: -30,
+      opacity: 0,
+      delay: 1,
+      duration: 0.8,
+      stagger: 0.1,
+    });
+  
+    tl2.from('.hero-section h1', {
+      x: -200,
+      opacity: 0,
+      duration: 0.5,
+    });
+  
+    tl2.from('.hero-section p', {
+      x: -300,
+      opacity: 0,
+      duration: 0.4,
+    });
+  
+    tl2.from('.hero-section .btn', {
+      opacity: 0,
+      duration: 0.4,
+    });
+  
+    tl2.from('.hero-section img', {
+      opacity: 0,
+      duration: 0.4,
+    }, "-=1"); 
+
+    tl2.from(".section1bottom img", {
+      opacity: 0,
+      y: 30,
+      stagger: 0.15,
+      duration: 0.6,
+    });
+  }
+  
+  page1animation();
+  
+  
+  // Scroll-triggered animation
+  const tl3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#topListDiv h1",
+      scroller: "body",
+      markers: false, 
+      start: "top 80%",
+      end: "top 60%",
+      scrub: 2,
+    }
+  });
+  
+  tl3.from("#topListDiv h1", {
+    y: 30,
     opacity: 0,
-    y: 10})
+    duration: 0.5
+  });
 
-    gsap.set("#shopping-img", {
+  tl3.from(".card", {
+    y: -30,
     opacity: 0,
-    y: 10,
-    scale: 0.8,})
-
-tl
-    .to(".animation", {
-        opacity: 1,
-        y: 0,
-        ease: Expo.easeInOut,
-        duration: 3.5,
-        stagger: 0.1,
-        delay: -1,
-    })
-
-    .to("#shopping-img", {
-        opacity: 1,
-        y: 0,
-        scale: 1,
-        ease: Expo.easeInOut,
-        duration: 2,
-        stagger: 1.1,
-        delay: -3,
-    })
+    duration: 1,
+    stagger:0.5,
+  },"sametime");
+  
